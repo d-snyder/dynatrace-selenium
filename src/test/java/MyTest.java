b14ce56b9acf76795e50de8c84248afc28d7fb57
= <code@dsnyder.com>
2014-11-03 11:29:20 -0500

    This is an experiment using the automation sdk to register a test so it gets picked up in the test automation view.

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,13 +1,20 @@
 /**
  * Created by drsnyder on 10/16/14.
  */
+import com.dynatrace.diagnostics.automation.rest.sdk.RESTEndpoint;
 import com.dynatrace.webautomation.DynaTraceWebDriver;
 import com.dynatrace.webautomation.DynaTraceWebDriverHelper;
+import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
+import com.thoughtworks.selenium.webdriven.commands.WaitForPageToLoad;
 import org.junit.*;
 import org.junit.rules.TestName;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -15,37 +22,39 @@ public class MyTest {
 
     protected static WebDriver driver = null;
     protected static DynaTraceWebDriverHelper dynaTrace = null;
+    protected static RESTEndpoint endPoint = null;
+    protected static String buildId = null;
+    private String testRunId = null;
 
+    //Set the timer name to match the test method name
+    @Before
+    public void beforeTest() {
+        buildId = System.getProperty("BUILD_ID");
+        String dt_username = System.getProperty("dt_username","admin");
+        String dt_password = System.getProperty("dt_password","admin");
+        endPoint = new RESTEndpoint(dt_username,dt_password,"http://localhost:8020");
+        testRunId = endPoint.startTest("1","1","1","1",buildId,"1","browser",null,"uidriven","Windowsx86_64",buildId,new HashMap<String, String>());
+        System.setProperty("testRunID",testRunId);
 
-    // Makes the current test name available inside the test methods
-    @Rule
-    public TestName name;
-    {
-        name = new TestName();
-    }
-
-
-    // Setup of the IE Driver and Dyantrace wrapper classes
-    @BeforeClass
-    public static void startUp() {
         DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
         ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
         driver = DynaTraceWebDriver.forWebDriver(new InternetExplorerDriver(ieCapabilities));
         dynaTrace = DynaTraceWebDriverHelper.forDriver(driver);
 
-        dynaTrace.enableTimerNames(false);
+        dynaTrace.enableTimerNames(true);
         dynaTrace.enableMarkers(false);
-    }
 
+        dynaTrace.setTimerName(testRunId);
+    }
 
     //Set the timer name to match the test method name
-    @Before
-    public void beforeTest() {
-        dynaTrace.setTimerName(this.getClass().getName(), name.getMethodName());
+    @After
+    public void afterTest() {
+        dynaTrace.setTimerName(null);
+        endPoint.endTest(testRunId);
+
     }
 
-
-    //Be nice and close the driver
     @AfterClass
     public static void tearDown() {
         if(driver != null) {
@@ -55,11 +64,23 @@ public class MyTest {
         }
     }
 
-
     //Finally, the test.  Simply open the page www.dynatrace.com
     @Test
     public void openDynatraceDotCom()   {
-        dynaTrace.setTestStepName("openPage");
-        driver.navigate().to("http://www.dynatrace.com");
+        try {
+            // Not sure if this is needed - Give the browser agent some time to connect.
+            Thread.sleep(10000);
+
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.navigate().to("http://dynatrace.com");
+            driver.findElement(By.linkText("SITEMAP"));
+
+            // To be safe, give the browser agent some time to send all it's data before IE is closed.
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
